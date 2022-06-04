5f2bdc3387b7290039b4ad394014bd9fb36942a8
Prasadkpd <kpdplakshan@gmail.com>
2022-06-04 16:12:25 +0530

    Customer.java done

---
+++ src/main/java/com/customer/CustomerDBUtil.java
@@ -0,0 +1,10 @@
+package com.customer;
+
+import java.util.List;
+
+public class CustomerDBUtil {
+    public static List<Customer> validate(String userName, String password) {
+
+        return null;
+    }
+}
